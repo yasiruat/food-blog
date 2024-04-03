fc3c34039e16b6dbf335e98271c96d2e162105c8
Yasiru Thanthirige <yasiruat.26@gmail.com>
2024-04-03 23:06:20 +0530

    CRUD

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 app.use(express.json());
@@ -10,6 +12,8 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch(err => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(5000, () => {
     console.log("Backend is running.");
